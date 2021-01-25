15b2015a03f3e20a4f0b2a045cae83c9a5810c7c
Burak Kırlaroğlu <52127496+burakkirlaroglu@users.noreply.github.com>
2021-01-25 09:29:25 +0300

    wiil add exception handling

--- src/main/java/org/kodluyoruz/mvc/userController.java
+++ src/main/java/org/kodluyoruz/mvc/userController.java
@@ -1,6 +1,8 @@
 package org.kodluyoruz.mvc;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,20 +18,29 @@ public class userController {
 
     private Map<UUID, User> users = new HashMap<>();
 
-    @RequestMapping(method = RequestMethod.POST)
-    public User save(@RequestBody UserDto userDto) {
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User save(@RequestBody UserDto userDto,
+                     @RequestHeader("Authorization") String authHeader) {
+
+        System.out.println(authHeader);
 
         User u = new User(UUID.randomUUID(),userDto.getName(),userDto.getAge());
         users.put(u.getId(),u);
         return u;
     }
 
-    @RequestMapping(value = "/{id}" ,method = RequestMethod.GET)
+    //@RequestMapping(value = "/{id}" ,method = RequestMethod.GET)
+    @GetMapping(value = "/{id}" ,produces = "application/json")
     public User get(@PathVariable UUID id){
        return users.get(id);
+        /*return users.get(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND), "User not found with id:"+id);*/
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<User> get(@RequestParam String name){
         return users.entrySet()
                 .stream()
